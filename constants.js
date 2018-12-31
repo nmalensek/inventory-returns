043b6a87fd3992d3524474365319aa8073c67a42
Nick Malensek <nick@malensek.net>
2018-12-31 10:32:19 -0800

    initial commit; first row basic functionality/appearance in place

---
+++ constants.js
@@ -0,0 +1,30 @@
+const itemContainerStyle = 'itemContainer';
+const departmentContainerStyle = 'departmentContainer';
+const lowerElementStyle = 'LowerElement';
+const leftColumnStyle = 'LeftColumn';
+const middleColumnStyle = 'MiddleColumn';
+const rightColumnStyle = 'RightColumn';
+const inputLabel = 'InputLabel';
+const linkStyle = 'LinkStyle';
+const alignRight = 'AlignRight';
+const leftPadded = 'LeftPadded';
+const applicableItem = 'ApplicableItem';
+const invalidSelection = 'InvalidSelection';
+
+const viewEditableOnly = 'viewEditableOnly';
+const viewApplicableOnly = 'viewApplicableOnly';
+const estimatedCosts = 'estimatedCosts';
+
+const otherReasonId = 'otherReasonText';
+
+const notApplicableSelections = {
+    0 : '-- Select --',
+    1 : 'No items produced this period',
+    2: 'No returns allowed (limited edition, etc.)',
+    3: 'Credit already counted in a prior period',
+    4: 'Seasonal',
+    5: 'Other (provide reason)'
+  };
+
+const defaultSelectValue = 0;
+const otherReasonValue = 5;
